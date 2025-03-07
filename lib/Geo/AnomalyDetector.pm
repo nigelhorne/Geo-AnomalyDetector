edd4786b614bc7b0850f1509bf263d4c513f358b
Nigel Horne <njh@bandsman.co.uk>
2025-03-07 08:06:43 -0500

    Use Math:Trig

--- lib/Geo/AnomalyDetector.pm
+++ lib/Geo/AnomalyDetector.pm
@@ -16,13 +16,13 @@ Geo::AnomalyDetector - Detect anomalies in geospatial coordinate datasets
 This module analyzes latitude and longitude data points to identify anomalies based on their distance from the mean location.
 
   use Geo::AnomalyDetector;
- 
+
   my $detector = Geo::AnomalyDetector->new(threshold => 3);
   my $coords = [ [37.7749, -122.4194], [40.7128, -74.0060], [35.6895, 139.6917] ];
   my $anomalies = $detector->detect_anomalies($coords);
   print "Anomalies: " . join ", ", map { "($_->[0], $_->[1])" } @{$anomalies};
 
-Each co-ordinate can be either a two eleement array of [latitude, longitude] or an object that has
+Each co-ordinate can be either a two element array of [latitude, longitude] or an object that has
 C<latitude> and C<longitude> methods.
 
 =head1	VERSION
@@ -52,12 +52,20 @@ sub detect_anomalies {
 
 	my $inverse = Geo::Inverse->new();
 
+	die if(!defined($mean_lat) || !defined($mean_lon));
+
 	foreach my $coord (@{$coordinates}) {
 		my ($lat, $lon) = (ref($coord) eq 'ARRAY') ? @{$coord} : ($coord->latitude(), $coord->longitude());
 		die if(!defined($lat) || !defined($lon));
-		my $distance = distance($lat, $lon, $mean_lat, $mean_lon, 'K');
-		# ::diag($distance, ', ', Math::Trig::great_circle_distance($lon, $lat, $mean_lon, $mean_lat), ', ',
-			# $inverse->inverse($lat, $lon, $mean_lat, $mean_lon));
+		# my $distance = distance($lat, $lon, $mean_lat, $mean_lon, 'K');
+
+		# Thanks to Robbie Hatley for working out the arguments to Math::Trig
+		my $t1 = $lon * (pi/180);
+		my $p1 = (pi/2) - ($lat * (pi/180));
+		my $t2 = $mean_lon * (pi/180);
+		my $p2 = (pi/2) - ($mean_lat * (pi/180));
+		my $rho = ($self->{'unit'} eq 'M') ? 3959.0 : 6371.0;	# radius of Earth in miles/Km
+		my $distance = Math::Trig::great_circle_distance($t1, $p1, $t2, $p2, $rho);
 		push @distances, $distance;
 	}
 
@@ -74,57 +82,59 @@ sub detect_anomalies {
 	return \@anomalies;
 }
 
-=head2 distance
+# Now use Math::Trig.  I tried Geo::Inverse, but that always throws messages about undefined variables
 
-Calculate the distance between two geographical points using latitude and longitude.
-Supports distance in kilometres (K), nautical miles (N), or miles.
-
-From L<http://www.geodatasource.com/developers/perl>
-FIXME:  use Math::Trig
-
-=cut
-
-sub distance {
-	my ($lat1, $lon1, $lat2, $lon2, $unit) = @_;
-	my $theta = $lon1 - $lon2;
-	my $dist = sin(_deg2rad($lat1)) * sin(_deg2rad($lat2)) + cos(_deg2rad($lat1)) * cos(_deg2rad($lat2)) * cos(_deg2rad($theta));
-	$dist = _acos($dist);
-	$dist = _rad2deg($dist);
-	$dist = $dist * 60 * 1.1515;
-	if ($unit eq 'K') {
-		$dist = $dist * 1.609344;	# number of kilometres in a mile
-	} elsif ($unit eq 'N') {
-		$dist = $dist * 0.8684;
-	}
-	return ($dist);
-}
-
-my $pi = atan2(1,1) * 4;
-
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#:::  This function get the arccos function using arctan function   :::
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-sub _acos {
-	my ($rad) = @_;
-	my $ret = atan2(sqrt(1 - $rad**2), $rad);
-	return $ret;
-}
-
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#:::  This function converts decimal degrees to radians			 :::
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-sub _deg2rad {
-	my ($deg) = @_;
-	return ($deg * $pi / 180);
-}
-
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#:::  This function converts radians to decimal degrees			 :::
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-sub _rad2deg {
-	my ($rad) = @_;
-	return ($rad * 180 / $pi);
-}
+# =head2 distance
+#
+# Calculate the distance between two geographical points using latitude and longitude.
+# Supports distance in kilometres (K), nautical miles (N), or miles.
+#
+# From L<http://www.geodatasource.com/developers/perl>
+# FIXME:  use Math::Trig
+#
+# =cut
+#
+# sub distance {
+	# my ($lat1, $lon1, $lat2, $lon2, $unit) = @_;
+	# my $theta = $lon1 - $lon2;
+	# my $dist = sin(_deg2rad($lat1)) * sin(_deg2rad($lat2)) + cos(_deg2rad($lat1)) * cos(_deg2rad($lat2)) * cos(_deg2rad($theta));
+	# $dist = _acos($dist);
+	# $dist = _rad2deg($dist);
+	# $dist = $dist * 60 * 1.1515;
+	# if ($unit eq 'K') {
+		# $dist = $dist * 1.609344;	# number of kilometres in a mile
+	# } elsif ($unit eq 'N') {
+		# $dist = $dist * 0.8684;
+	# }
+	# return ($dist);
+# }
+#
+# my $pi = atan2(1,1) * 4;
+#
+# #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# #:::  This function get the arccos function using arctan function   :::
+# #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# sub _acos {
+	# my ($rad) = @_;
+	# my $ret = atan2(sqrt(1 - $rad**2), $rad);
+	# return $ret;
+# }
+#
+# #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# #:::  This function converts decimal degrees to radians			 :::
+# #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# sub _deg2rad {
+	# my ($deg) = @_;
+	# return ($deg * $pi / 180);
+# }
+#
+# #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# #:::  This function converts radians to decimal degrees			 :::
+# #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# sub _rad2deg {
+	# my ($rad) = @_;
+	# return ($rad * 180 / $pi);
+# }
 
 =head1 AUTHOR
 
@@ -134,7 +144,11 @@ Nigel Horne, C<< <njh at nigelhorne.com> >>
 
 =head1 SEE ALSO
 
-L<Geo::Location::Point>
+=over 4
+
+=item * L<Geo::Location::Point>
+
+=item * L<Math::Trig>
 
 =head1 SUPPORT
 
